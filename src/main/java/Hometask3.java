b2e2121a10275c72eec2d334515e336717bffab8
oxanakapcova <117301767+oxanakapcova@users.noreply.github.com>
2023-03-11 13:52:17 +0300

    Update Hometask3.java

--- src/main/java/Hometask3.java
+++ src/main/java/Hometask3.java
@@ -50,7 +50,6 @@ public class Hometask3 {
         for (int i = 0; i < yourArray.size()+count; i++) {
             newList.add(whatToAdd);
         }
-        long end = System.currentTimeMillis();
     return newList;
     }
 
